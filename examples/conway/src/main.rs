46fc01f0bb16edae3a3f3c78414c563f2dd11e6d
Jacob Millward <jacob@millward.dev>
2024-01-01 14:16:40 +0000

    Update winit to 0.29 in Conway example

--- examples/conway/src/main.rs
+++ examples/conway/src/main.rs
@@ -6,8 +6,9 @@ use log::{debug, error};
 use pixels::{Error, Pixels, SurfaceTexture};
 use winit::{
     dpi::LogicalSize,
-    event::{Event, VirtualKeyCode},
-    event_loop::{ControlFlow, EventLoop},
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    keyboard::KeyCode,
     window::WindowBuilder,
 };
 use winit_input_helper::WinitInputHelper;
@@ -17,7 +18,7 @@ const HEIGHT: u32 = 300;
 
 fn main() -> Result<(), Error> {
     env_logger::init();
-    let event_loop = EventLoop::new();
+    let event_loop = EventLoop::new().unwrap();
     let mut input = WinitInputHelper::new();
 
     let window = {
@@ -42,99 +43,107 @@ fn main() -> Result<(), Error> {
 
     let mut draw_state: Option<bool> = None;
 
-    event_loop.run(move |event, _, control_flow| {
-        // The one and only event that winit_input_helper doesn't have for us...
-        if let Event::RedrawRequested(_) = event {
-            life.draw(pixels.frame_mut());
-            if let Err(err) = pixels.render() {
-                log_error("pixels.render", err);
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-        }
-
-        // For everything else, for let winit_input_helper collect events to build its state.
-        // It returns `true` when it is time to update our game state and request a redraw.
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-            if input.key_pressed(VirtualKeyCode::P) {
-                paused = !paused;
-            }
-            if input.key_pressed_os(VirtualKeyCode::Space) {
-                // Space is frame-step, so ensure we're paused
-                paused = true;
-            }
-            if input.key_pressed(VirtualKeyCode::R) {
-                life.randomize();
-            }
-            // Handle mouse. This is a bit involved since support some simple
-            // line drawing (mostly because it makes nice looking patterns).
-            let (mouse_cell, mouse_prev_cell) = input
-                .mouse()
-                .map(|(mx, my)| {
-                    let (dx, dy) = input.mouse_diff();
-                    let prev_x = mx - dx;
-                    let prev_y = my - dy;
-
-                    let (mx_i, my_i) = pixels
-                        .window_pos_to_pixel((mx, my))
-                        .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
-
-                    let (px_i, py_i) = pixels
-                        .window_pos_to_pixel((prev_x, prev_y))
-                        .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
-
-                    (
-                        (mx_i as isize, my_i as isize),
-                        (px_i as isize, py_i as isize),
-                    )
-                })
-                .unwrap_or_default();
-
-            if input.mouse_pressed(0) {
-                debug!("Mouse click at {mouse_cell:?}");
-                draw_state = Some(life.toggle(mouse_cell.0, mouse_cell.1));
-            } else if let Some(draw_alive) = draw_state {
-                let release = input.mouse_released(0);
-                let held = input.mouse_held(0);
-                debug!("Draw at {mouse_prev_cell:?} => {mouse_cell:?}");
-                debug!("Mouse held {held:?}, release {release:?}");
-                // If they either released (finishing the drawing) or are still
-                // in the middle of drawing, keep going.
-                if release || held {
-                    debug!("Draw line of {draw_alive:?}");
-                    life.set_line(
-                        mouse_prev_cell.0,
-                        mouse_prev_cell.1,
-                        mouse_cell.0,
-                        mouse_cell.1,
-                        draw_alive,
-                    );
-                }
-                // If they let go or are otherwise not clicking anymore, stop drawing.
-                if release || !held {
-                    debug!("Draw end");
-                    draw_state = None;
-                }
-            }
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                    log_error("pixels.resize_surface", err);
-                    *control_flow = ControlFlow::Exit;
+    event_loop
+        .run(move |event: Event<()>, event_loop| {
+            // The one and only event that winit_input_helper doesn't have for us...
+            if let Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } = &event
+            {
+                life.draw(pixels.frame_mut());
+                if let Err(err) = pixels.render() {
+                    log_error("pixels.render", err);
+                    event_loop.exit();
                     return;
                 }
             }
-            if !paused || input.key_pressed_os(VirtualKeyCode::Space) {
-                life.update();
+
+            // For everything else, for let winit_input_helper collect events to build its state.
+            // It returns `true` when it is time to update our game state and request a redraw.
+            if input.update(&event) {
+                // Close events
+                if input.key_pressed(KeyCode::Escape) || input.close_requested() {
+                    event_loop.exit();
+                    return;
+                }
+                if input.key_pressed(KeyCode::KeyP) {
+                    paused = !paused;
+                }
+                if input.key_pressed_os(KeyCode::Space) {
+                    // Space is frame-step, so ensure we're paused
+                    paused = true;
+                }
+                if input.key_pressed(KeyCode::KeyR) {
+                    life.randomize();
+                }
+                // Handle mouse. This is a bit involved since support some simple
+                // line drawing (mostly because it makes nice looking patterns).
+                let (mouse_cell, mouse_prev_cell) = input
+                    .cursor()
+                    .map(|(mx, my)| {
+                        let (dx, dy) = input.mouse_diff();
+                        let prev_x = mx - dx;
+                        let prev_y = my - dy;
+
+                        let (mx_i, my_i) = pixels
+                            .window_pos_to_pixel((mx, my))
+                            .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
+
+                        let (px_i, py_i) = pixels
+                            .window_pos_to_pixel((prev_x, prev_y))
+                            .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
+
+                        (
+                            (mx_i as isize, my_i as isize),
+                            (px_i as isize, py_i as isize),
+                        )
+                    })
+                    .unwrap_or_default();
+
+                if input.mouse_pressed(0) {
+                    debug!("Mouse click at {mouse_cell:?}");
+                    draw_state = Some(life.toggle(mouse_cell.0, mouse_cell.1));
+                } else if let Some(draw_alive) = draw_state {
+                    let release = input.mouse_released(0);
+                    let held = input.mouse_held(0);
+                    debug!("Draw at {mouse_prev_cell:?} => {mouse_cell:?}");
+                    debug!("Mouse held {held:?}, release {release:?}");
+                    // If they either released (finishing the drawing) or are still
+                    // in the middle of drawing, keep going.
+                    if release || held {
+                        debug!("Draw line of {draw_alive:?}");
+                        life.set_line(
+                            mouse_prev_cell.0,
+                            mouse_prev_cell.1,
+                            mouse_cell.0,
+                            mouse_cell.1,
+                            draw_alive,
+                        );
+                    }
+                    // If they let go or are otherwise not clicking anymore, stop drawing.
+                    if release || !held {
+                        debug!("Draw end");
+                        draw_state = None;
+                    }
+                }
+                // Resize the window
+                if let Some(size) = input.window_resized() {
+                    if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                        log_error("pixels.resize_surface", err);
+                        event_loop.exit();
+                        return;
+                    }
+                }
+                if !paused || input.key_pressed_os(KeyCode::Space) {
+                    life.update();
+                }
+                window.request_redraw();
             }
-            window.request_redraw();
-        }
-    });
+        })
+        .expect("event loop exited with error");
+
+    Ok(())
 }
 
 fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
